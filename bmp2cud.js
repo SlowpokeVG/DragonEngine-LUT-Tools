15895546063bb21fc003c8890f4330464708430a
SlowpokeVG <screamer.jr@gmail.com>
2021-02-02 10:52:23 +0300

    Y7 Support
    Added support for v2 cud files

--- bmp2cud.js
+++ bmp2cud.js
@@ -3,15 +3,28 @@ const path = require("path");
 
 let arguments = process.argv.slice(2);
 if (arguments.length < 1)
-    throw "Not enough arguments. Pass name of the file"
+    throw "Not enough arguments. Pass name of the file and '-v2' if you are working with Y7+ LUTs'"
 
-let fileName = arguments[0];
+let inputPath = arguments[0];
+let version = arguments[1];
+
 const cudHeader = Buffer.from('10000000FFFFFFFF0000', 'hex');
+const cudHeader20 = Buffer.from('20000000FFFFFFFF0000', 'hex');
 
-if (fileName.indexOf('bmp') != -1) 
+if (fs.lstatSync(inputPath).isDirectory())
+fs.readdirSync(inputPath).forEach(file => {
+    bmpConvert(path.join(inputPath, file), version);
+})
+else bmpConvert(inputPath, version);
+
+function bmpConvert(filePath, ver)
 {
-        convertFile = fs.readFileSync(fileName).slice(56);
-        console.log(fileName);
-        fs.writeFileSync(fileName + ".cud", cudHeader);
-        fs.writeFileSync(fileName + ".cud", convertFile, { flag: "a" });
-};
+    if (path.extname(filePath) == '.bmp') {
+        convertFile = fs.readFileSync(filePath).slice(56);
+        filePath = filePath.slice(0, -4);
+        console.log(filePath);
+        if (ver == "-v2") fs.writeFileSync(filePath + ".cud", cudHeader20);
+        else fs.writeFileSync(filePath + ".cud", cudHeader);
+        fs.writeFileSync(filePath + ".cud", convertFile, { flag: "a" });
+    }
+}
--- cud2bmp.js
+++ cud2bmp.js
@@ -5,17 +5,28 @@ let arguments = process.argv.slice(2);
 if (arguments.length < 1)
     throw "Not enough arguments. Pass name of the folder"
 
-let folderName = arguments[0];
-const bmpHeader = Buffer.from('424D36300000000000003600000028000000000100001000000001001800000000000030000000000000000000000000000000000000', 'hex');
-const cudHeader = Buffer.from('10000000FFFFFFFF', 'hex');
+let inputPath = arguments[0];
 
-fs.readdirSync(folderName).forEach(file => {
-    if (file.indexOf('cud') != -1) {
-        const fileName = path.parse(file).name;
-        convertFile = fs.readFileSync(path.join(folderName, file)).slice(8);
-        console.log(path.join(folderName, file));
-        fs.writeFileSync(path.join(folderName, fileName) + ".bmp", bmpHeader);
-        fs.writeFileSync(path.join(folderName, fileName) + ".bmp", convertFile, { flag: "a" });
+const bmpHeader10 = Buffer.from('424D36300000000000003600000028000000000100001000000001001800000000000030000000000000000000000000000000000000', 'hex');
+const bmpHeader20 = Buffer.from('424D36300000000000003600000028000000000400002000000001001800000000000030000000000000000000000000000000000000', 'hex');
+
+if (fs.lstatSync(inputPath).isDirectory())
+fs.readdirSync(inputPath).forEach(file => {
+    cudConvert(path.join(inputPath, file));
+})
+else cudConvert(inputPath);
+
+function cudConvert(filePath)
+{
+    if (path.extname(filePath) == '.cud') {
+        convertFile = fs.readFileSync(filePath);
+        let ver = convertFile[0];
+        convertFile = convertFile.slice(8);
+        filePath = filePath.slice(0, -4);
+        console.log(filePath);
+        if (ver == 0x10) fs.writeFileSync(filePath + ".bmp", bmpHeader10);
+        else if (ver == 0x20) fs.writeFileSync(filePath + ".bmp", bmpHeader20);
+        else throw('Unknown version of CUD' + ver)
+        fs.writeFileSync(filePath + ".bmp", convertFile, { flag: "a" });
     }
-
-});
+}
